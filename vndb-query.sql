94ca400599c9913634ebf3b31c096908b1578df2
Otniel113 <otniel.abiezer.saragih@gmail.com>
2022-10-28 11:44:50 +0700

    Menambah lagi

--- vndb-query.sql
+++ vndb-query.sql
@@ -11,6 +11,10 @@ FROM releases_vn rv
 JOIN vn v ON rv.vid = v.id
 JOIN releases r ON rv.id = r.id;
 
+-- =====================  Count Total User of VNDB ===================== 
+SELECT COUNT(id)
+FROM users;
+
 -- =====================  Non H Anime Adapted From Visual Novel ===================== 
 SELECT v.title AS "vn_title", a.title_romaji AS "anime_title", a.type, a.year AS "anime_year", MIN(r.released) AS "vn_released_date", 'https://anidb.net/anime/' || a.id AS "anime_link", 'https://vndb.org/' || v.id AS "vn_link"
 FROM vn v
@@ -31,6 +35,25 @@ WHERE v.title = 'Summer Pockets'
 AND (notes <> '' OR vote IS NOT NULL)
 ORDER BY added DESC;
 
--- =====================  Count Total User of VNDB ===================== 
-SELECT COUNT(id)
-FROM users;
+-- =====================  VN in Indonesian Language ===================== 
+SELECT v.title, r.released, v.original, v.olang AS "original_language", r.website
+FROM releases_lang rlang
+JOIN releases r ON rlang.id = r.id
+JOIN releases_vn rvn ON r.id = rvn.id
+JOIN vn v ON v.id = rvn.vid
+-- You can change this to specific the language
+WHERE lang='id'
+ORDER BY v.c_popularity DESC;
+
+-- =====================  Numbers of VN by Platform ===================== 
+SELECT platform, COUNT(id)
+FROM releases_platforms
+GROUP BY platform
+ORDER BY COUNT(id) DESC;
+
+-- =====================  Numbers of VN by Minimum Ages ===================== 
+SELECT minage, COUNT(r.title)
+FROM releases_vn rv
+JOIN vn v ON rv.vid = v.id
+JOIN releases r ON rv.id = r.id
+GROUP BY minage;
